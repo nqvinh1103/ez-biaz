1575f1b90fab80812e39179ce394c18e596dbf72
huy <sliverzack1@gmail.com>
2026-03-27 16:49:37 +0700

    chore(api): remove unused feature services after CQRS migration

--- backend/src/EzBias.Application/Common/Interfaces/Services/IContactService.cs
+++
@@ -1,6 +0,0 @@
-namespace EzBias.Application.Common.Interfaces.Services;
-
-public interface IContactService
-{
-    Task SendAsync(string name, string email, string message, CancellationToken cancellationToken = default);
-}
